1e787a3b4947e24d842095c520db648acf921d6a
rhuandelfino <127902637+rhuandelfino@users.noreply.github.com>
2024-12-12 15:48:58 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Contador from './contador';
+
+function App() {
+    return (
+        <div className="App">
+            <Contador /> {/* Utilizando o componente */}
+        </div>
+    );
+}
+
+export default App;
